d0fa83074c4b2fb3eb28858156b5daf037b8166b
61130061 <thezeroyou@gmail.com>
2022-06-30 15:06:23 +0700

    update styles

---
+++ src/components/NavbarNote.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+
+function AddTagButton () {
+   const [hover, setHover] = useState(false);
+
+   return (
+      <div 
+         className="flex gap-1 items-center rounded-lg hover:cursor-pointer"
+         onMouseOver={() => setHover(true)}
+         onMouseLeave={() => setHover(false)}
+      >
+         <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} color={hover ? "#fff[" : "#d4d4d8"}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z" />
+         </svg>
+         <div className={hover ? "text-xs text-white" : "text-xs text-zinc-400"}>Add tag</div>
+      </div>
+   )
+}
+
+function Button ({ children }) {
+   const [hover, setHover] = useState(false);
+
+   return (
+      <div 
+         className="p-1 hover:cursor-pointer"
+         onMouseOver={() => setHover(true)} 
+         onMouseLeave={() => setHover(false)}
+      >
+         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} color={hover ? "#fff[" : "#d4d4d8"}>
+            {children}
+         </svg>
+      </div>
+   )
+}
+
+
+export default function NavbarNote () {
+   return (
+      <div className="flex justify-between text-sm mb-10">
+         <div className="flex gap-3 items-center font-bold">
+            <div className="flex gap-2 items-center">
+               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
+               </svg>
+               <div className="hover:underline hover:cursor-pointer underline-offset-4">
+                  My Folder
+               </div>
+            </div>
+            <AddTagButton />
+         </div>
+
+         <div className="flex gap-3">
+            <Button>
+                 <path strokeLinecap="round" strokeLinejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+            </Button>
+            <Button>
+               <path strokeLinecap="round" strokeLinejoin="round" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
+            </Button>
+            <Button>
+               <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
+            </Button>
+         </div>
+      </div>
+   )
+}
--- src/pages/Note.jsx
+++ src/pages/Note.jsx
@@ -1,6 +1,8 @@
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
+import NavbarNote from '../components/NavbarNote';
+
 const markdown = `
 # h1
 ## h2
@@ -9,6 +11,12 @@ const markdown = `
 A paragraph with *emphasis* and **strong importance**.
 
 > A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+
+## Paragraph
+**Lorem Ipsum** is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+
+Testing code tag: \`test\`
 
 * Lists
 * [ ] todo
@@ -22,36 +30,27 @@ A table:
 | col 2 is      | centered      |   $12 |
 | zebra stripes | are neat      |    $1 |
 
+
+Code:
+
+\`\`\`js
+const hello = 'Hello world';
+
+console.log(hello);
+\`\`\`
 `
-
 export default function Note () {
-   return (
-      <div className="flex flex-col flex-1 p-5">
-         {/* Navigation bar */}
-         <div className="flex justify-between text-sm mb-10">
-            <div className="flex gap-3 items-center font-bold">
-               <div className="flex gap-3 items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                     <path strokeLinecap="round" strokeLinejoin="round" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
-                  </svg>
-                  <div className="hover:underline hover:cursor-pointer underline-offset-4">My Folder</div>
-               </div>
-               <div className="text-zinc-400">/</div>
-               <div>Untitle note</div>
+  return (
+     <div className="flex flex-col flex-1 p-3">
+        <NavbarNote />
+
+        <div className="self-center md:w-[600px]">
+           <div>
+              <div className="text-4xl font-bold my-5">Untitled note</div>
+               <div className="text-zinc-300 font-bold">discription...</div>
             </div>
 
-            <div>
-               <div>3 dots menu</div>
-            </div>
-         </div>
-
-         <div className="self-center md:w-[600px]">
-            <div>
-               <div className="text-4xl font-bold my-5">Untitled note</div>
-               <div className="text-gray-400 font-bold">discription...</div>
-            </div>
-
-            <article className="my-10 text-lg font-medium leading-8">
+            <article className="my-10 text-md">
                <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />
             </article>
 
