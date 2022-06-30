876148ea702d8f62318321857c647aa7ff2c2267
61130061 <thezeroyou@gmail.com>
2022-06-30 13:43:04 +0700

    update coverted markdown style

--- src/pages/Note.jsx
+++ src/pages/Note.jsx
@@ -1,6 +1,29 @@
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
+const markdown = `
+# h1
+## h2
+### h3
+
+A paragraph with *emphasis* and **strong importance**.
+
+> A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+
+* Lists
+* [ ] todo
+* [x] done
+
+A table:
+
+| Tables        | Are           | Cool  |
+| ------------- |:-------------:| -----:|
+| col 3 is      | right-aligned | $1600 |
+| col 2 is      | centered      |   $12 |
+| zebra stripes | are neat      |    $1 |
+
+`
+
 export default function Note () {
    return (
       <div className="flex flex-col flex-1 p-5">
@@ -28,9 +51,9 @@ export default function Note () {
                <div className="text-gray-400 font-bold">discription...</div>
             </div>
 
-            <div className="my-10 text-lg font-medium leading-8">
-               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </div>
+            <article className="my-10 text-lg font-medium leading-8">
+               <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />
+            </article>
 
          </div>
 
